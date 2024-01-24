e9e219aba169d82b3da49cb7fb66e95a6d89b9f9
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-01-24 16:58:35 +0700

    feat(js): add sample variable

---
+++ javascript-sample/variable.js
@@ -0,0 +1,15 @@
+var a = 3;
+var b = 10;
+
+let x = 'x';
+let y = '2';
+
+const d = 'aaa';
+const dd = '100';
+
+console.log('a: ' + a);
+console.log('b: ' + b);
+console.log('x: ' + x)
+console.log('y: ' + y);
+console.log('d: ' + d);
+console.log('dd: ' + dd);
