c3ca829bce2021f2ff5f9aa113b4a75a2b62c6b3
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-01-31 16:24:03 +0700

    feat(js): add sample about spread and object assign

--- javascript-sample/dataTypes.js
+++ javascript-sample/dataTypes.js
@@ -10,7 +10,7 @@ console.log(object);
 
 // Immutable
 let immutableString = 'Hello';
-immutableString[1] = 'a'; //error
+// immutableString[1] = 'a'; //error
 let newString = immutableString + ' World';
 
 let immutableNumber = 42;
@@ -19,3 +19,24 @@ let newNumber = immutableNumber + 10;
 console.log(immutableString);
 console.log(newString)
 console.log(newNumber);
+
+// Use the spread operator to a new copy of copy array 
+const originalArray = [1, 2, 3];
+const copyArray = [...originalArray];
+
+// Use the spread operator to create a new copy of the object
+const originalObject = { a: 1, b: 2 };
+
+// Object.assign copy properties from originalObject
+const copyObject = Object.assign({}, originalObject); 
+
+copyArray.push(4);
+copyObject.c = 3;
+
+console.log(originalArray);
+console.log(copyArray); 
+
+console.log(originalObject);
+console.log(copyObject);
+
+
