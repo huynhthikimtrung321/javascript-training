77f7b7d36c9b46f0b7a9aca345d1993eed3fd920
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-02-01 11:01:17 +0700

    feat(js): add sample for method slice

--- javascript-sample/arrays.js
+++ javascript-sample/arrays.js
@@ -1,21 +1,26 @@
 // Declaration
 
-let languages = ["PHP", "JavaScript", "C#"];
+let languages = ['PHP', 'JavaScript', 'C#', 'Ruby', 'Python'];
 console.log(languages[0]);
 
 //add the new one to the array
-languages[3] = "Java";
+languages[3] = 'Java';
 
 // How to add an item to an array
-languages.push("C++");
+languages.push('C++');
 
 // How to remove an item from an array
 languages.shift();
 
 // How to join two or more arrays
 
-let framework = ["Wicket", "Angular", "Gulp"];
+let framework = ['Wicket', 'Angular', 'Gulp'];
 let arr = languages.concat(framework);
+
+// How to cut part of an array into a new array
+
+let cutLanguages = languages.slice(2);
+console.log(cutLanguages);
 
 //Access element of an array
 console.log(`[${languages}][0] = ${languages[0]}`);
@@ -36,7 +41,6 @@ console.log(result);
 
 // filter
 let check = num.filter(element => element <25)
-
 console.log(check);
 
 
