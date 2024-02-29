029de20f14b15dc3e948f82d443b327f08b49753
huynhthikimtrung321 <106976157+huynhthikimtrung321@users.noreply.github.com>
2024-02-29 15:16:15 +0700

    Feature/implement show task (#29)
    * feat(js): update file package.js and gitignore
    
    * feat(js): implement show task
    
    * fix(js): move file db.json
    
    * feat(api): add api
    
    * feat(api): api
    
    * fix(js): create .env file and save json-server to dev
    
    * fix(js): update file app.js
    
    * fix(js): define constant error messages
    
    * fix(js): move file and create env sample
    
    * fix(readme): update file readme
    
    * fix(typo): spelling correction

---
+++ practice-one/src/js/app.js
@@ -0,0 +1,31 @@
+import { get } from "./services/apis.js";
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const tasks = (await get()).data;
+
+  renderTasks(tasks);
+});
+
+function renderTasks(tasks) {
+  const listElement = document.querySelector('.todo-list');
+
+  tasks.forEach(task => {
+    const itemElement = document.createElement('li');
+    const viewDiv = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+    const button = document.createElement('button');
+
+    itemElement.className = 'todo-item';
+    viewDiv.className = 'view';
+    checkbox.type = 'checkbox';
+    checkbox.className = 'toggle-item';
+    label.textContent = task.name;
+    label.className = 'todo-item-label';
+    button.className = 'btn-destroy';
+
+    viewDiv.append(checkbox, label, button);
+    itemElement.append(viewDiv);
+    listElement.prepend(itemElement);
+  });
+}
