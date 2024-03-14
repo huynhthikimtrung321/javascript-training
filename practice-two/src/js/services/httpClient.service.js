8773a44b51c14c468e1911c4ef268d24002b1d33
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-03-14 15:28:42 +0700

    fix(api): define a HttpService

--- practice-two/src/js/services/httpClient.service.js
+++ practice-two/src/js/services/httpClient.service.js
@@ -1,69 +1,79 @@
 import axios from 'axios';
 import { ERROR_MESSAGES } from '../constants/messages';
 
-async function get(endpoint) {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/${endpoint}`);
-
-    if (!res.ok) {
-      throw new Error (ERROR_MESSAGES.GET_FAILED_MSG);
-    }
-
-    return response.data;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-async function post(endpoint) {
-  try {
-    const response = await axios.post(`${API_BASE_URL}/${endpoint}`, {
+class HttpService {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+    this.axiosClient = axios.create({
+      baseURL: baseUrl,
       headers: {
         'Content-Type': 'application/json',
       },
     });
+  }
 
-    if (!res.ok) {
-      throw new Error (ERROR_MESSAGES.POST_FAILED_MSG);
+  async get(endpoint) {
+    try {
+      const response = await this.axiosClient.get(endpoint);
+
+      if (!res.ok) {
+        throw new Error (ERROR_MESSAGES.GET_FAILED_MSG);
+      }
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
     }
+  }
 
-    return response.data;
-  } catch (error) {
-    console.error(error.message);
+  async post(endpoint) {
+    try {
+      const response = await this.axiosClient.post(`${API_BASE_URL}/${endpoint}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error (ERROR_MESSAGES.POST_FAILED_MSG);
+      }
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async edit(endpoint) {
+    try {
+      const response = await this.axiosClient.patch(`${API_BASE_URL}/${endpoint}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error (ERROR_MESSAGES.EDIT_FAILED_MSG);
+      }
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async delete (endpoint) {
+    try {
+      await this.axiosClient.delete(`${API_BASE_URL}/${endpoint}`);
+
+      if (!res.ok) {
+        throw new Error (ERROR_MESSAGES.DELETE_FAILED_MSG);
+      }
+
+      return response.data;
+
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 }
-
-async function edit(endpoint) {
-  try {
-    const response = await axios.patch(`${API_BASE_URL}/${endpoint}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!res.ok) {
-      throw new Error (ERROR_MESSAGES.EDIT_FAILED_MSG);
-    }
-
-    return response.data;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-async function deleted(endpoint) {
-  try {
-    await axios.delete(`${API_BASE_URL}/${endpoint}`);
-
-    if (!res.ok) {
-      throw new Error (ERROR_MESSAGES.DELETE_FAILED_MSG);
-    }
-
-    return response.data;
-
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-export { get, post, edit, deleted };
