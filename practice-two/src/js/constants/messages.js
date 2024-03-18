bda7c8ba7bd6bbe79c91f0424e47d9b21b74386a
huynhthikimtrung321 <106976157+huynhthikimtrung321@users.noreply.github.com>
2024-03-18 14:31:43 +0700

    feat(api): Initialize data to mock api (#46)
    * feat(init): initialization structure
    
    * feat(init): npm install axios
    
    * fix(init): install eslint
    
    * fix(init): add file and folder
    
    * fix(init): update file readme and editorconfig
    
    * fix(init): rename and remove file
    
    * fix(init): update file eslint
    
    * fix(readme): update readme
    
    * feat(css): create header
    
    * feat(css): update header
    
    * feat(image): move image file
    
    * feat(css): create form
    
    * feat(css): create table layout
    
    * feat(api): Initialize data to mock api
    
    * fix(package): remove key 'main'
    
    * feat(api): Create httpClient.service.js: get, post, put, delete
    
    * fix(api): define a HttpService
    
    * fix(service): update file httpClient.service.js
    
    * fix(api): add payload data to post and patch

---
+++ practice-two/src/js/constants/messages.js
@@ -0,0 +1,8 @@
+const ERROR_MESSAGES = {
+  GET_FAILED_MSG: 'Getting data is failed!',
+  POST_FAILED_MSG: 'Posting data is failed!',
+  EDIT_FAILED_MSG: 'Updating data is failed!',
+  DELETE_FAILED_MSG: 'Deleting data is failed!'
+}
+
+export { ERROR_MESSAGES };
---
+++ practice-two/src/js/services/httpClient.service.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+export default class HttpService {
+  constructor() {
+    this.baseUrl = process.env.BASE_API_URL;
+    this.axiosClient = axios.create({
+      baseURL: this.baseUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  async get(endpoint) {
+    try {
+      const response = await this.axiosClient.get(`${endpoint}`);
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async post(endpoint, product) {
+    try {
+      const response = await this.axiosClient.post(endpoint, product);
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async patch(endpoint, product) {
+    try {
+      const response = await this.axiosClient.patch(endpoint, product);
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async delete (endpoint) {
+    try {
+      await this.axiosClient.delete(endpoint);
+
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+}
