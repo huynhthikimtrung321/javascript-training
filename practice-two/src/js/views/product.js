46b9af9bdd22007e390de387281c8ca5501144c0
huynhthikimtrung321 <beohuynh321@gmail.com>
2024-03-23 17:12:22 +0700

    feat(view):  implement function bindAddProduct

--- practice-two/src/js/views/product.js
+++ practice-two/src/js/views/product.js
@@ -1,11 +1,22 @@
 import icon from '../../assets/images/icon.svg';
+import {
+  isNotEmptyField,
+  hasMinLength,
+  isValidSKU,
+  isNumber,
+  isInteger,
+  isPositiveNumber,
+  renderErrorMessages,
+  validateForm
+} from '../helpers/validateForm';
 
 export default class ProductView {
   displayProducts(products) {
     const mainContent = document.getElementById('product-list');
     mainContent.innerHTML = '';
 
-    let listItemHTML = '<ul class="table-header">'
+    let listItemHTML = '<ul class="table-header">';
+    console.log(products)
     products?.map(products => {
       const {
         name,
@@ -116,7 +127,7 @@ export default class ProductView {
       if (event.key !== 'Enter') return;
 
       const searchValue = event.target.value.toLowerCase();
-      const searchedProducts = await handleSearchProductByKeyword({name: searchValue});
+      const searchedProducts = await handleSearchProductByKeyword({ name: searchValue });
       this.displayProducts(searchedProducts);
     })
   }
@@ -132,8 +143,8 @@ export default class ProductView {
 
       const filterValues = {};
 
-      if(activeValue) filterValues.status = activeValue;
-      if(categoryValue) filterValues.category = categoryValue;
+      if (activeValue) filterValues.status = activeValue;
+      if (categoryValue) filterValues.category = categoryValue;
 
       handleFilterProducts(filterValues);
     })
@@ -143,7 +154,7 @@ export default class ProductView {
     const mainContent = document.querySelector('.main-content');
     mainContent.addEventListener('click', (event) => {
       const target = event.target;
-      if(!target.dataset.sortLabel) return;
+      if (!target.dataset.sortLabel) return;
       const targetSiblings = Array.from(target.parentNode.children);
       targetSiblings.filter(sibling => sibling !== target).map(sibling => {
         sibling.classList.remove('arrow-up');
@@ -153,14 +164,14 @@ export default class ProductView {
       const isArrowDown = target.classList.contains('arrow-down');
       const isArrowUp = target.classList.contains('arrow-up');
 
-      if(!isArrowDown && !isArrowUp) {
+      if (!isArrowDown && !isArrowUp) {
         target.classList.add('arrow-down');
         handleSortProduct(target.dataset.field, 'desc');
-      } else if(isArrowDown) {
+      } else if (isArrowDown) {
         target.classList.remove('arrow-down');
         target.classList.add('arrow-up');
         handleSortProduct(target.dataset.field, 'asc');
-      } else if(isArrowUp) {
+      } else if (isArrowUp) {
         target.classList.remove('arrow-up');
         handleSortProduct(target.dataset.field, '');
       }
@@ -173,41 +184,50 @@ export default class ProductView {
 
     const formProductHTML = `
       <div class="modal-overlay hidden">
-        <form action="" method="post" class="add-product-container">
+        <form action="javascript:void(0)" method="post" class="add-product-container">
           <h2 class="add-product-title">Add products</h2>
           <div class="form-group">
             <label for="name">Product Name:</label>
-            <input type="text" id="name" name="name" placeholder="Enter product name" class="form-input">
-          </div>
+            <input id="name" data-field-name="Name" name="name" placeholder="Enter product name" class="form-input">
+            <p class="error-msg" data-field-error="Name"></p>
+            </div>
           <div class="form-group">
             <label for="category">Category:</label>
-            <select id="category" name="category" class="form-input">
+            <select id="category" data-field-name="category" class="form-input">
               <option>Skin care</option>
               <option>Face care</option>
               <option>Lips care</option>
             </select>
           </div>
           <div class="form-group">
+            <label for="sku">SKU:</label>
+            <input type="text" id="sku" data-field-name="SKU"class="form-input">
+            <p class="error-msg" data-field-error="SKU"></p>
+          </div>
+          <div class="form-group">
             <label for="quantity">Quantity:</label>
-            <input type="text" id="quantity" name="quantity" placeholder="0" class="form-input">
+            <input id="quantity" data-field-name="Quantity" name="quantity" placeholder="0" class="form-input">
+            <p class="error-msg" data-field-error="Quantity"></p>
           </div>
           <div class="form-group">
             <label for="price">Price:</label>
-            <input type="text" id="price" name="price" placeholder="Enter price" class="form-input">
+            <input id="price" data-field-name="Price" name="price" placeholder="Enter price" class="form-input">
+            <p class="error-msg" data-field-error="Price"></p>
           </div>
           <div class="form-group">
             <label for="cost">Cost:</label>
-            <input type="text" id="cost" name="cost" placeholder="Enter cost" class="form-input">
+            <input id="cost" data-field-name="Cost" name="cost" placeholder="Enter cost" class="form-input">
+            <p class="error-msg" data-field-error="Cost"></p>
           </div>
           <div class="form-group">
             <label for="status">Status:</label>
-            <select id="status" name="status" class="form-input">
+            <select id="status" data-field-name="status" class="form-input">
               <option value="active">Active</option>
               <option value="inactive">Inactive</option>
             </select>
           </div>
           <div>
-            <input type="submit" value="Add Product" class="add-product-submit">
+            <input type="submit" value="Add Product" class="add-product-submit" id="btn-add-product">
           </div>
         </form>
       </div>
@@ -215,8 +235,8 @@ export default class ProductView {
     mainContent.innerHTML += formProductHTML;
 
     const modalOverlay = document.querySelector('.modal-overlay');
-    modalOverlay.addEventListener('click', (event) => {
-      if(event.target === modalOverlay) {
+    modalOverlay.addEventListener('mousedown', (event) => {
+      if (event.target === modalOverlay) {
         modalOverlay.classList.toggle('hidden');
       }
     });
@@ -227,6 +247,94 @@ export default class ProductView {
         modalOverlay.classList.toggle('hidden');
       }
     });
+  }
+
+  removeModal() {
+    const modalOverlay = document.querySelector('.modal-overlay');
+    modalOverlay.classList.toggle('hidden');
+  }
+
+  bindAddProduct(handleAddProduct) {
+    const mainContent = document.querySelector('.main-content');
+    mainContent.addEventListener('click', (event) => {
+      const target = event.target;
+      if (target.id !== 'btn-add-product') return;
+
+      const formElement = document.querySelector('.add-product-container');
+      const nameInputElement = formElement.querySelector('[data-field-name="Name"]');
+      const categoryInputElement = formElement.querySelector('[data-field-name="category"]');
+      const statusInputElement = formElement.querySelector('[data-field-name="status"]');
+      const skuInputElement = formElement.querySelector('[data-field-name="SKU"]')
+      const quantityInputElement = formElement.querySelector('[data-field-name="Quantity"]');
+      const priceInputElement = formElement.querySelector('[data-field-name="Price"]');
+      const costInputElement = formElement.querySelector('[data-field-name="Cost"]');
+
+      const formFields = [
+        {
+          field: 'Name',
+          value: nameInputElement.value,
+          validators: [
+            isNotEmptyField,
+            hasMinLength
+          ]
+        },
+        {
+          field: 'SKU',
+          value: skuInputElement.value,
+          validators: [
+            isNotEmptyField,
+            isValidSKU
+          ]
+        },
+        {
+          field: 'Quantity',
+          value: quantityInputElement.value,
+          validators: [
+            isNotEmptyField,
+            isInteger,
+            isPositiveNumber
+          ]
+        },
+        {
+          field: 'Price',
+          value: priceInputElement.value,
+          validators: [
+            isNotEmptyField,
+            isNumber,
+            isPositiveNumber
+          ]
+        },
+        {
+          field: 'Cost',
+          value: costInputElement.value,
+          validators: [
+            isNotEmptyField,
+            isNumber,
+            isPositiveNumber
+          ]
+        }
+      ];
+
+      const formError = validateForm(formFields);
+      for (let key in formError) {
+        renderErrorMessages(formElement, {});
+        if (formError[key] !== '') {
+          return renderErrorMessages(formElement, formError);
+        }
+      }
+
+      const product = {
+        name: nameInputElement.value,
+        category: categoryInputElement.value,
+        sku: skuInputElement.value,
+        quantity: quantityInputElement.value,
+        price: priceInputElement.value,
+        cost: costInputElement.value,
+        status: statusInputElement.value
+      }
+
+      handleAddProduct(product);
+    })
   }
 
 }
